8607bd316f18c1f82178e4c778ebe8fabf93072a
Schlabach <matthewshawn89@gmail.com>
2023-02-15 06:39:51 -0500

    Delete Products (5)

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -14,6 +14,8 @@ namespace Testing
         public void InsertProduct(Product productToInsert);
         public IEnumerable<Category> GetCategories();
         public Product AssignCategory();
+        public void DeleteProduct(Product product);
+
 
 
     }
